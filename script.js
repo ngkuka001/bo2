d8a328fcff73d443f1192b6fea3fc7917ba67575
van.nguyen <van.nguyen@s3corp.com.vn>
2020-06-30 18:11:25 +0700

    update

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 var modal = document.getElementById("myModal");
 var btnComplete = document.querySelector(".btn-complete");
-var btnReward = document.querySelectorAll('.progress-item[aria-active="true"] .btn-reward');
-btnComplete.addEventListener('click', function() {
+var btnReward = document.querySelectorAll('.progress-item .btn-reward');
+btnComplete.addEventListener('click', function () {
     modal.classList.add('hidden')
 })
 
-btnReward.forEach(element => {
-    element.addEventListener('click', function() {
-        modal.classList.remove('hidden');
+btnReward.forEach(button => {
+    button.addEventListener('click', function () {
+        if(this.classList.contains('active')) {
+            modal.classList.remove('hidden');
+            this.classList.remove('active');
+        }
     })
 })
